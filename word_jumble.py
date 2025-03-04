386c5685c6a03e506c7abc28cb6c4367c28c7eac
FrazierMark <m.mark.frazier@gmail.com>
2025-03-04 15:41:28 +0900

    Add loop to ask for multiple jumbled words.

--- word_jumble.py
+++ word_jumble.py
@@ -1,15 +1,23 @@
 import random
-from collections import Counter
-
-# Unscrambles 1 word at a time.
 
 dictionary = open("/usr/share/dict/words", "r")
 dictionary_list = [word.strip() for word in dictionary.readlines()]
 dictionary.close()
 
 
+def word_to_dict(word_to_descramble):
+  letters_to_num_letters = {}
+  
+  # create the dict letter to quantity: {'t': 3, 'r': 1}
+  for letter in word_to_descramble:
+    letter_count = letters_to_num_letters.get(letter, 0)
+    letter_count += 1
+    letters_to_num_letters[letter] = letter_count
+    
+  return letters_to_num_letters
+
 def ask_for_jumble():
-  jumble = input("Type in you word jumble please.")
+  jumble = input("Type in you word jumble please: \n")
   return jumble
 
 def find_possible_len_words(letter_count_dict, scrambled_word_length):
@@ -17,17 +25,25 @@ def find_possible_len_words(letter_count_dict, scrambled_word_length):
     
     for word in dictionary_list:
         if len(word) == scrambled_word_length:  # Compare to actual word length
-            possible_answer = dict(Counter(word))  # Letter frequency of dict word
+            possible_answer = dict(word_to_dict(word))  # Letter frequency of dict word
             if possible_answer == letter_count_dict:
                 possible_words.append(word)
     
     return possible_words
 
+def ask_for_number_of_words():
+    num_words = int(input("How many words would you like to descramble? "))
+    return num_words
 
-word_to_descramble = ask_for_jumble()
-letter_count_dict = dict(Counter(word_to_descramble))  # Letter count of scrambled word
+num_words = ask_for_number_of_words()  # Ask for the number of words to descramble
+all_possible_words = []  # List to store possible words for all scrambled inputs
 
-possible_words = find_possible_len_words(letter_count_dict, len(word_to_descramble))
+for _ in range(num_words):
+    word_to_descramble = ask_for_jumble()  # Get each scrambled word
+    letter_count_dict = dict(word_to_dict(word_to_descramble))  # Letter count of scrambled word
+    possible_words = find_possible_len_words(letter_count_dict, len(word_to_descramble))  # Find possible words
+    all_possible_words.append(possible_words)  # Store the possible words for this input
 
-print("Possible words:", possible_words)
+print("Possible words for each input:", all_possible_words)  # Print all possible words
+
 
