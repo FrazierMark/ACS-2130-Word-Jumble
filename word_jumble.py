338b4f7622a7f167a6586d849b73e61b351913ef
FrazierMark <m.mark.frazier@gmail.com>
2025-03-04 15:42:48 +0900

    rmv comments

--- word_jumble.py
+++ word_jumble.py
@@ -8,7 +8,7 @@ dictionary.close()
 def word_to_dict(word_to_descramble):
   letters_to_num_letters = {}
   
-  # create the dict letter to quantity: {'t': 3, 'r': 1}
+  # create the dict: letter (key) to quantity (value): {'t': 3, 'r': 1}
   for letter in word_to_descramble:
     letter_count = letters_to_num_letters.get(letter, 0)
     letter_count += 1
@@ -24,8 +24,8 @@ def find_possible_len_words(letter_count_dict, scrambled_word_length):
     possible_words = []
     
     for word in dictionary_list:
-        if len(word) == scrambled_word_length:  # Compare to actual word length
-            possible_answer = dict(word_to_dict(word))  # Letter frequency of dict word
+        if len(word) == scrambled_word_length:
+            possible_answer = dict(word_to_dict(word))
             if possible_answer == letter_count_dict:
                 possible_words.append(word)
     
@@ -35,15 +35,15 @@ def ask_for_number_of_words():
     num_words = int(input("How many words would you like to descramble? "))
     return num_words
 
-num_words = ask_for_number_of_words()  # Ask for the number of words to descramble
+num_words = ask_for_number_of_words()
 all_possible_words = []  # List to store possible words for all scrambled inputs
 
 for _ in range(num_words):
-    word_to_descramble = ask_for_jumble()  # Get each scrambled word
-    letter_count_dict = dict(word_to_dict(word_to_descramble))  # Letter count of scrambled word
+    word_to_descramble = ask_for_jumble()
+    letter_count_dict = dict(word_to_dict(word_to_descramble))
     possible_words = find_possible_len_words(letter_count_dict, len(word_to_descramble))  # Find possible words
-    all_possible_words.append(possible_words)  # Store the possible words for this input
+    all_possible_words.append(possible_words)
 
-print("Possible words for each input:", all_possible_words)  # Print all possible words
+print("Possible words for each input:", all_possible_words)
 
 
